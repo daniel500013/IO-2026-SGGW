626468eeefb0bbe176286e064c30b5618fe06013
Daniel Szeląg <bakucraft229@gmail.com>
2026-04-29 19:19:37 +0200

    [IO] Podmiana nazwy głównego formularza

--- IO_2026_SGGW/MainForm.Designer.cs
+++ IO_2026_SGGW/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IO_2026_SGGW
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- IO_2026_SGGW/Program.cs
+++ IO_2026_SGGW/Program.cs
@@ -16,7 +16,7 @@ namespace IO_2026_SGGW
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
