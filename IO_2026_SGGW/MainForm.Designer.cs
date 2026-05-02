0356bcf7d6b7f0aa4aa91592466151c637ec460e
SugierM <michal.sugier.off@gmail.com>
2026-05-02 11:33:35 +0200

    Dodano ListBox zgodnie ze specyfikacją.

--- IO_2026_SGGW/MainForm.Designer.cs
+++ IO_2026_SGGW/MainForm.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstResults = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lstResults
+            // 
+            this.lstResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstResults.BackColor = System.Drawing.Color.White;
+            this.lstResults.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstResults.Font = new System.Drawing.Font("Consolas", 9F);
+            this.lstResults.ForeColor = System.Drawing.Color.Black;
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.HorizontalScrollbar = true;
+            this.lstResults.IntegralHeight = false;
+            this.lstResults.ItemHeight = 14;
+            this.lstResults.Location = new System.Drawing.Point(12, 130);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(700, 325);
+            this.lstResults.TabIndex = 0;
+            this.lstResults.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 471);
+            this.Controls.Add(this.lstResults);
             this.Name = "MainForm";
             this.Text = "Sprawdzanie Kolokwiów";
             this.ResumeLayout(false);
@@ -42,6 +63,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstResults;
     }
 }
 
--- IO_2026_SGGW/MainForm.cs
+++ IO_2026_SGGW/MainForm.cs
@@ -16,5 +16,10 @@ namespace IO_2026_SGGW
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
